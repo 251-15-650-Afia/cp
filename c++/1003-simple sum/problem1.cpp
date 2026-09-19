788dfa6a786cb00a25b4d9eee9f866a2673dbba9
251-15-650 Afia <251-15-650@diu.edu.bd>
2026-09-20 00:08:34 +0600

    updaTED

---
+++ c++/1003-simple sum/problem1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  int numA,numB;
+
+  cin>> numA>> numB;
+
+  int SOMA = numA+numB;
+
+  cout<< "SOMA = " << SOMA <<endl;
+
+  return 0;
+
+
+
+
+
+}
