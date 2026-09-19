70a01b0692f4dc95b75637ed5a67fe0c315b378a
251-15-650 Afia <251-15-650@diu.edu.bd>
2026-09-19 23:59:44 +0600

    added

---
+++ c++/Problem solve/problem1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a,b;
+
+    cin >> a >> b;
+
+
+    double ans= (a*3.5+b*7.5)/11.0;
+
+    cout << "MEDIA = " << fixed << setprecision(5) << ans << endl;
+
+
+    return 0;
+}
