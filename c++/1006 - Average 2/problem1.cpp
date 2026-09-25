6c6c31a5ea0c9c5f134e8b0fd02d16fb4c48c423
251-15-650 Afia <251-15-650@diu.edu.bd>
2026-09-26 02:39:43 +0600

    asdasd

---
+++ c++/1006 - Average 2/problem1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+     double a,b,c; // correct
+
+     cin >> a >> b >> c;
+     double aa=((((a*2))+(b*3)+(c*5)))/10;
+
+     cout << "MEDIA = " << fixed << setprecision(1) << aa << endl;
+
+     return 0;
+}
